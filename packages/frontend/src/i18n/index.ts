31b1d62c2e1e5947f6df006a3ef499af53b3feb6
Federico Zivolo <federico.zivolo@gmail.com>
2026-01-24 13:14:19 +0100

    add french and simplified chinese translations

--- packages/frontend/src/i18n/index.ts
+++ packages/frontend/src/i18n/index.ts
@@ -2,12 +2,16 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './locales/en';
+import fr from './locales/fr';
 import it from './locales/it';
+import zhHans from './locales/zh-Hans';
 
 export const defaultNS = 'common';
 export const resources = {
   en,
+  fr,
   it,
+  'zh-Hans': zhHans,
 } as const;
 
 i18n.use(initReactI18next).init({
